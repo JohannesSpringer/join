89937c8af4cd513f87e2c466163dd08336394075
Johannes Springer <johannes.springer.01@gmx.de>
2023-07-22 16:43:57 +0200

    start adding dropdown category

--- js/add_task.js
+++ js/add_task.js
@@ -8,13 +8,41 @@ function getDate() {
 }
 
 function renderAddTask() {
-    document.getElementById('addTaskInputs').innerHTML += getInputTitle();
+    document.getElementById('addTaskInputsLeft').innerHTML += genHtmlInputTitle();
+    document.getElementById('addTaskInputsLeft').innerHTML += genHtmlInputDescription();
+    document.getElementById('addTaskInputsLeft').innerHTML += genHtmlInputCategory();
+    document.getElementById('reqTaskTitle').style.color = 'red';
 }
 
-function getInputTitle() {
+function genHtmlInputTitle() {
     return `<div class="task-title">
                 Title
                 <input type="text" id="taskTitle" placeholder="Enter a title" required>
                 <span id="reqTaskTitle">This field is required</span>
             </div>`;
+}
+
+function genHtmlInputDescription() {
+    return `<div class="task-description">
+                Description
+                <textarea id="taskDescription" placeholder="Enter a Description" rows="4" required></textarea>
+                <span id="reqTaskDescription">This field is required</span>
+            </div>`;
+}
+
+function genHtmlInputCategory() {
+    return `<div class="task-category">
+                Category
+                <select id="taskCategory" required>
+                    <option value="def">default</option>
+                    ${getHtmlCategoryOptions()}
+                </select>
+                <span id="reqTaskCategory">This field is required</span>
+            </div>`;
+}
+
+function getHtmlCategoryOptions() {
+    return `<option value="1">1</option>
+            <option value="2">2</option>
+    `;
 }
